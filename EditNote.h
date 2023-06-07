bbbcc699063f0ce5946e774a3b8cffd7669e69d5
shreyakamath92 <135786579+shreyakamath92@users.noreply.github.com>
2023-06-06 22:37:56 -0400

    Create EditNote.h

---
+++ EditNote.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#ifndef EDITNOTE_H
+#define EDITNOTE_H
+
+#include "Note.h"
+#include "AddNote.h"
+
+class EditNote {
+  public: 
+  // constructor
+  EditNote();
+  // editing notes function, 4 parameters
+  void editNotes(vector<Note>&, int, int, string);
+  
+  // deconstructor
+  ~EditNote() {};
+};
+
+#endif
