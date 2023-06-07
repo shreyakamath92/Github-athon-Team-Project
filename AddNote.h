d81889b4779a949d099b36735d2a6087e29d5300
shreyakamath92 <135786579+shreyakamath92@users.noreply.github.com>
2023-06-06 22:35:00 -0400

    Create AddNote.h

---
+++ AddNote.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#ifndef ADDNOTE_H
+#define ADDNOTE_H
+
+#include "Note.h"
+
+using namespace std;
+
+class AddNote {
+
+  private:
+    
+
+  public:
+  // constructor
+  AddNote();
+  // vector storing user notes
+  vector<Note>notes;
+  vector<string>titles;
+  // functions
+  void addNote(Note);
+  void printNotes();
+  void titlesVector(Note);
+  // sorting functions
+  void sortByTime(vector<Note>&);
+  void sortByTitle(vector<Note>&);
+
+  string convertTimes(time_t);
+  // decontructor
+  ~AddNote() {};
+};
+#endif
