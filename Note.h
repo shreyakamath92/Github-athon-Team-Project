66dc559933c153eb0939c0b17a399b5b429e33ca
shreyakamath92 <135786579+shreyakamath92@users.noreply.github.com>
2023-06-06 22:32:47 -0400

    Create Note.h

---
+++ Note.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#ifndef NOTE_H
+#define NOTE_H
+
+using namespace std;
+
+class Note {
+  private:
+    // private variables
+    time_t noteTime;
+    string noteTitle;
+    string noteContent;
+
+
+  public:  
+    // public methods
+    // constructors
+    Note();
+    Note(time_t, string, string);
+
+    // getter functions
+    time_t getTime();
+    string getTitle();
+    string getContent();
+    // setter functions
+    void setTitle(string);
+    void setContent(string);
+
+    // deconstructor
+    ~Note() {};
+};
+
+#endif
