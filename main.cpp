7327f0a0f16af53538a75874058d420013203353
shreyakamath92 <135786579+shreyakamath92@users.noreply.github.com>
2023-06-09 06:48:41 -0400

    Commit main.cpp
    Main

--- main.cpp
+++ main.cpp
@@ -20,7 +20,6 @@ int main() {
   // declaring variables
   int menuChoice, choice2, choice3, noteCounter, noteNum1, choice4, noteNum2;
   string title, content, content2, title2;
-  // variable to determine time
   time_t now;
 
   // creating objects
@@ -30,26 +29,24 @@ int main() {
   DeleteNote del = DeleteNote();
   findNote find = findNote();
 
-  // counting amount of notes
-  noteCounter = 0;
-
   do {
     // displaying menu options
-    cout << "Welcome to Notesify. Choose an option from the menu below to begin: \n\t 1. Add Note\n\t 2. Edit Note\n\t 3. Delete Note\n\t 4. Display Existing Notes\n\t 5. Find a Note \n\t6. Quit \nChoice: "; cin >> menuChoice;
+    cout << "Welcome to Notesify. Choose an option from the menu below to begin: \n\t 1. Add Note\n\t 2. Edit Note\n\t 3. Delete Note\n\t 4. Display Existing Notes\n\t 5. Find a Note \n\t 6. Quit \nChoice: "; cin >> menuChoice;
 
+    //Error Trap
     while(1) {
-        if(menuChoice < 1 || menuChoice > 5) {
-          cout << "Invalid option. Do you want to: \n\t 1. Add Note\n\t 2. Edit Note\n\t 3. Delete Note\n\t 4. Display Existing Notes\n\t 5. Quit\nChoice: "; cin >> menuChoice;
+        if((menuChoice >= 1 && menuChoice <= 6) && !cin.fail()) {
+          break;
+        }
+        if(menuChoice < 1 || menuChoice > 6) { 
+          cout << "Invalid option. Do you want to:  \n\t 1. Add Note\n\t 2. Edit Note\n\t 3. Delete Note\n\t 4. Display Existing Notes\n\t 5. Find a Note \n\t 6. Quit \nChoice: "; cin >> menuChoice;
         }
         if(cin.fail()) {
           cin.clear();
           cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
-          cout << "Invalid Input. Please enter a NUMBER: \n\t 1. Add Note\n\t 2. Edit Note\n\t 3. Delete Note\n\t 4. Display Existing Notes\n\t 5. Quit\nChoice: "; cin >> menuChoice;
+          cout << "Invalid Input. Please enter a NUMBER: \n\t 1. Add Note\n\t 2. Edit Note\n\t 3. Delete Note\n\t 4. Display Existing Notes\n\t 5. Find a Note \n\t 6. Quit \nChoice: "; cin >> menuChoice;
         }
-        if((menuChoice >= 1 && menuChoice <= 5) && !cin.fail()) {
-          break;
-        }
-      }
+    } //End Error Trap
 
     switch (menuChoice) {
     // adding note case
@@ -74,8 +71,12 @@ int main() {
       add.printNotes();
       // allowing user to choose note title/content to edit
       cout << "Which note would you like to edit?: "; cin >> noteNum1;
+      
        //Error Trap
       while(1) {
+        if ((noteNum1 >= 1 && noteNum1 <= add.notes.size()) && !cin.fail()) {
+          break;
+        }
         if (noteNum1 < 1 || noteNum1 > add.notes.size()) {
           cout << "Invalid note number. Please enter which note would you like to edit?: "; cin >> noteNum1;
         }
@@ -84,13 +85,15 @@ int main() {
           cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
           cout << "Invalid Input. Please enter a NUMBER. Which note would you like to edit?: "; cin >> noteNum1;
         }
-        if ((noteNum1 > 1 && noteNum1 < add.notes.size()) && !cin.fail()) {
-          break;
-        }
-      }
+      } //Error Trap End
+      
       cout << "Do you want to edit the title (1) or the content (2)?: "; cin >> choice4;
+      
       // error Trap
       while(1) {
+        if((choice4 == 1 || choice4 == 2) && !cin.fail()) {
+          break;
+        }
         if(choice4 != 1 || choice4 != 2) {
           cout << "Invalid note number. Do you want to edit the title (1) or the content (2)?: "; cin >> choice4;
         }
@@ -99,10 +102,8 @@ int main() {
           cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
           cout << "Invalid Input. Please enter a NUMBER. Do you want to edit the title (1) or the content (2)?: "; cin >> choice4;
         }
-        if((choice4 == 1 || choice4 == 2) && !cin.fail()) {
-          break;
-        }
-      }
+      } //End error trap
+      
       cout << "Start Typing Here: "; getline(cin >> ws, content2); 
       // calling function
       edit.editNotes(add.notes, noteNum1, choice4, content2);
@@ -119,20 +120,22 @@ int main() {
       add.printNotes();
       // user choice of note to delete
       cout << "Which note would you like to delete?: "; cin >> noteNum2;
+      
       //Error Trap
       while(1) {
+        if ((noteNum2 >= 1 && noteNum2 <= add.notes.size()) && !cin.fail()) {
+          break;
+        }
         if (noteNum2 < 1 || noteNum2 > add.notes.size()) {
-          cout << "Invalid note number. Please enter which note would you like to edit?: "; cin >> noteNum2;
+          cout << "Invalid note number. Please enter which note would you like to delete?: "; cin >> noteNum2;
         }
         if(cin.fail()) {
           cin.clear();
           cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
-          cout << "Invalid Input. Please enter a NUMBER. Please enter which note would you like to edit?: "; cin >> noteNum2;
+          cout << "Invalid Input. Please enter a NUMBER. Please enter which note would you like to delete?: "; cin >> noteNum2;
         }
-        if ((noteNum2 >= 1 && noteNum2 <= add.notes.size()) && !cin.fail()) {
-          break;
-        }
-      }
+      } //end error trap
+      
       del.deleteNotes(add.notes, noteNum2);
       cout << "Updated Notes:\n";
       add.printNotes();
@@ -146,7 +149,10 @@ int main() {
       // allowing user to choose how notes are displayed in console
       cout << "Display Notes\n\t1) Alphabetically\n\t2) Chronologically?\nChoice: "; cin >> choice2;
 
-      while(1) {
+      while(1) { //Error trap
+        if((choice2 == 1 || choice2 == 2) && !cin.fail()) {
+          break;
+        }
         if(choice2 != 1 || choice2 != 2) {
           cout << "Invalid option. Do you want to display notes Alphabetically (1) or Chronologically(2)? Choice: "; cin >> choice2;
         }
@@ -155,11 +161,9 @@ int main() {
           cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
           cout << "Invalid Input. Please enter a NUMBER. Do you want to display notes Alphabetically (1) or Chronologically(2)? Choice: "; cin >> choice2;
         }
-        if((choice2 == 1 || choice2 == 2) && !cin.fail()) {
-          break;
-        }
-      }
-      
+      } //end error trap
+
+      //switch sorting option
        switch(choice2) {
         case 1: {
           cout << "\n";
@@ -188,9 +192,11 @@ int main() {
     }
     break;
 
+    //finding note case
     case 5: {
-      cout << "Enter the title of the note you would like to find: "; cin >> title2;
-      find.look(title2);
+      cout << "Enter the title of the note you would like to find: "; getline(cin >> ws, title2); 
+      cout << title2;
+      find.look(add.notes, title2);
       find.whenFound(add.notes);
     }
     break;
@@ -212,13 +218,13 @@ int main() {
   } // End of switch statement
     
   // choice for user to continue choosing from the menu
-  cout << "\nEnter any number to choose from the menu again? If not, enter 0 to quit: "; cin >> choice3;
+  cout << "\nEnter any number to choose from the menu again. If not, enter 0 to quit: "; cin >> choice3;
   while(1) {
+    if(!cin.fail())
+      break;
     if(cin.fail()) {
       cout << "Invalid Input. Please enter a NUMBER: "; cin >> choice3;
     }
-    if(!cin.fail())
-      break;
   }
     
   }
